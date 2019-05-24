cc65ef460bd8319bbecce792a0aa8c43190d2991
HuangStanley050 <33140495+HuangStanley050@users.noreply.github.com>
2019-05-24 19:07:12 +1000

    stuck
    not able to set it up following the guide

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
-import React from "react"
+import React from "react";
+//import CMS from "netlify-cms-app";
 
-export default () => <div>Hello world!</div>
+export default () => {
+  //CMS.init();
+  return <div>Hello world!</div>;
+};
